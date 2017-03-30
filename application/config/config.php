df5594cc00d4de8253148b546563af25632fe6b7
hemantnyadav <hemantnyadav@gmail.com>
2017-03-30 22:33:54 +0530

    FIX: Added white list url for AJAX

--- application/config/config.php
+++ application/config/config.php
@@ -453,7 +453,7 @@ $config['csrf_token_name'] = 'csrf_himmat';
 $config['csrf_cookie_name'] = 'csrf_himmat_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array("home/populateToStations");
 
 /*
 |--------------------------------------------------------------------------
