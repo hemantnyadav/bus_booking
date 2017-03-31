a694a9746dabe8ba1c5b0f0eb0aa2dd575c0901f
hemantnyadav <hemantnyadav@gmail.com>
2017-04-01 01:18:23 +0530

    FIX: Update getBusList and index methods getbuslist now work for listing "to" data

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,7 +7,8 @@ class Home extends CI_Controller {
 		$this->load->model('home_model');
 		
 		$data['fromStations'] = $this->home_model->getFromStations();
-	
+		
+		
 		$this->load->view('header');
 		$this->load->view('home_view',$data);
 		$this->load->view('footer');
@@ -21,6 +22,33 @@ class Home extends CI_Controller {
 	
 		echo json_encode($data["toStations"]);
 	}
+	public function getListOfBus()
+	{
+		if(isset($_POST['fromStation']) && $_POST['fromStation']!="From" && (!isset($_POST['toStation']) || $_POST['toStation']=="To"))
+		{
+			$this->load->model('home_model');
+			$fromStation = $_POST['fromStation'];
+			$data["fromStationSelected"] = $fromStation;
+			
+			$data['fromStations'] = $this->home_model->getFromStations();
+			$data["toStations"] = $this->home_model->getToStations($fromStation);
+			
+			$this->load->view('header');
+			$this->load->view('home_view',$data);
+			$this->load->view('footer');			
+			
+		}else
+		{
+		
+		$data['fromStationVal']  = $_POST['fromStation'];
+		$data['toStationVal'] 	 = $_POST['toStation'];
+		$data['dateVal'] 		 = $_POST['date'];
+		
+		$this->load->view('header');
+		$this->load->view('bus_list_view',$_POST);
+		$this->load->view('footer');			
+		}
+	}	
 }
 
 /* End of file welcome.php */
