dcb7ddf846b6f3ef2135c75b312c008071cd8b61
hemantnyadav <hemantnyadav@gmail.com>
2017-04-01 14:23:54 +0530

    Feature: Complete layout and passanger details

--- application/views/bus_list_view.php
+++ application/views/bus_list_view.php
@@ -4,7 +4,7 @@
 		<div class="row">
             <div class="box">
                 <div class="col-lg-12 text-center">
-                    <div class="col-lg-6 text-center">
+                    <div class="col-lg-4 text-center">
 						
 						<style>
 							.seat
@@ -69,26 +69,74 @@
 									if(($j==2 || $j==3) && $i<5)
 										echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;".$seatNumber[$i][$j]."</td>";
 									else
-										echo "<td class='seat ". $UpperLower[$i][$j]."'><input type='checkbox' /><br>".$seatNumber[$i][$j]."</td>";
+										echo "<td class='seat ". $UpperLower[$i][$j]."'><input type='checkbox' value='".$seatNumber[$i][$j]."' /><br>".$seatNumber[$i][$j]."</td>";
 								}
 								echo "</tr>";
 							}
 						?>
 						</table>
+						<input type="checkbox" value=""
 					</div>
 				
-					<div class="col-lg-6 text-center">
-						<h3>
-						<?PHP echo $toStation."-".$fromStation." to ".$toStation."-".$date; ?></h3>
-						From Station
-						To Station
-						Bus Type
-						Date
-						Main Passanger Name
-						Main Passanger Contact
-						Main Passanger Email
-						
-						
+					<div class="col-lg-8 text-center">
+						<?PHP
+							$attributes = array('name'=>'fromto','id'=>'fromto');
+							$hidden 	= array('is_submit'=>1);
+							echo form_open("home/confirmTicket/",$attributes, $hidden); 
+						?>
+							<div class="clearfix"><br /></div>
+							<div class="col-lg-3 text-center">
+								 From <input type="text" class="form-control" value="<?PHP echo $fromStation;  ?>" disabled="disabled" />
+							</div>
+							<div class="col-lg-3 text-center">
+								To <input type="text" class="form-control" value="<?PHP echo $toStation;  ?>"  disabled="disabled">
+							</div>
+							<div class="col-lg-3 text-center">
+								Date <input type="text" class="form-control" value="<?PHP echo $date;  ?>" disabled="disabled" />
+							</div>
+							<div class="col-lg-2 text-center">
+								Ac/NonAC<input type="text" class="form-control" value="<?PHP echo $busType;  ?>" disabled="disabled" />
+							</div>
+							<div class="clearfix"></div>
+							<br /><br />
+							
+								<div class="row">
+									<div class="form-group col-lg-3">
+										<label>Passenger Name*</label>
+									</div>
+									<div class="form-group col-lg-5">
+										<input type="text" class="form-control">
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col-lg-3">
+										<label>Contact Number</label>
+									</div>
+									<div class="form-group col-lg-5">
+										<input type="text" class="form-control">
+									</div>
+								</div>
+								<div class="row">
+										<div class="form-group col-lg-3">
+										<label>Email ID</label>
+									</div>
+									<div class="form-group col-lg-5">
+										<input type="text" class="form-control">
+									</div>
+									</div>
+									<div class="clearfix"></div>
+									<div class="form-group col-lg-12" >
+
+										<textarea class="form-control" rows="6" disabled="disabled">Note: It is compulsory to carry ID card of Main Passenger.By Submiting this you agree with temrs and conditions of website usage. For cancellation call 02845-223715.
+										
+										</textarea>
+									</div>
+									<div class="form-group col-lg-12">
+										<input type="hidden" name="save" value="contact">
+										<button type="submit" class="btn btn-default">Book Ticket</button>
+									</div>
+								</div>
+						<?PHP echo form_close(); ?>
 					</div>
 				</div>
             </div>
