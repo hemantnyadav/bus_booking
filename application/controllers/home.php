86fc872aaeb17ba3c78d749f3abb514e6b507334
hemantnyadav <hemantnyadav@gmail.com>
2017-04-02 17:58:42 +0530

    Feature: Added bus layout listing and bus confirmation methods

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -49,6 +49,28 @@ class Home extends CI_Controller {
 		$this->load->view('footer');			
 		}
 	}	
+	public function confirmTicket()
+	{
+		$this->load->model('home_model');
+		
+		$data['busNo'] 			 = $this->home_model->getBusNo($_POST['busType']);
+		$data['busRoute']		 = $this->home_model->getBusRoute($_POST['busType']);
+		$data['ticketNumbers']   = $_POST['ticketNumbers'];
+		$data['pName'] 	 		 = $_POST['pName'];
+		$data['pContact'] 		 = $_POST['pContact'];
+		$data['pEmail'] 		 = $_POST['pEmail'];
+		
+		$data['fromStationVal']  = $_POST['fromStation'];
+		$data['toStationVal'] 	 = $_POST['toStation'];
+		$data['dateVal'] 		 = $_POST['date'];
+		$data['busTypeVal'] 	 = $_POST['busType'];
+		
+		$data["status"] = $this->home_model->addBooking($data);
+		
+		$this->load->view('header');
+		$this->load->view('confirmTicket',$data);
+		$this->load->view('footer');			
+	}
 }
 
 /* End of file welcome.php */
