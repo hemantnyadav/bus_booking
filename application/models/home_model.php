2b57d8ef9a94af91e1b265bda719708bd832bc69
hemantnyadav <hemantnyadav@gmail.com>
2017-04-02 18:00:19 +0530

    Feature: added method for busSeat retrival

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -29,4 +29,47 @@ class Home_model extends CI_Model {
 		$toStations = array_map("unserialize", array_unique(array_map("serialize", $toStations_merge)));
 		return $toStations;
 	}
+	public function getBusNo($busType)
+	{
+		$sql = "select bus_number from himmat.bus_service where bus_type = '".trim($busType)."'";
+		$busNo = $this->db->query($sql);
+		return $busNo->result_array();
+	}
+	public function	getBusRoute($busType)
+	{
+		$sql = "select * from routes where route_name in (select bus_route_name from himmat.bus_service where bus_type = '".trim($busType)."')";
+		$routeDetails = $this->db->query($sql);
+		return $routeDetails->result_array();
+	}
+	public function addBooking($data = array())
+	{
+		$bus_number = $data['busNo'];
+		$bus_number = $bus_number[0]['bus_number'];
+		
+		$ticketNumber = $data['ticketNumbers'];
+		$ticketNumber = implode(",",$ticketNumber);
+		
+		$busRoute = $data['busRoute'];
+		$busRoute = $busRoute[0]['route_station'];
+		
+		$sql = "insert into booking (booking_from,booking_to,booking_bus_no,booking_seats,booking_date,booking_route,booking_passanger,booking_passanger_contact,booking_passanger_email,booking_status)
+values(
+'".$data['fromStationVal']."', 
+'".$data['toStationVal']."',
+'".$bus_number."',
+'".$ticketNumber."',
+'".$data['dateVal']."',
+'".$busRoute."',
+'".$data['pName']."',
+'".$data['pContact']."',
+'".$data['pEmail']."',
+'blocked')";
+
+	//$query		= $this->db->query($sql);
+	if($this->db->query($sql))
+	return "Success";
+	else
+	return "Failed. Please try again!";
+	
+	}
 }
